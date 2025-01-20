24a6ed3fe6c694a470944c20c91cc150789d8132
AzizBenAmara19 <azizbenamara1920@gmail.com>
2025-01-20 10:16:12 +0100

    check.py

--- check.py
+++ check.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
+from sklearn.externals import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
